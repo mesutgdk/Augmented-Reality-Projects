88cd8c6f877a83497dbcbda18d909d5e4dfef980
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-05-06 20:35:46 +0300

    Add files via upload
    i got a flying DICE!

--- 3D-Dicee-Onboard/3D-Dicee-Onboard/ViewController.swift
+++ 3D-Dicee-Onboard/3D-Dicee-Onboard/ViewController.swift
@@ -18,15 +18,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+//
+//        // Show statistics such as fps and timing information
+//        sceneView.showsStatistics = true
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
+
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+//        let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true)
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            
+            diceNode.position = SCNVector3(0, 0, -0.1)
+            
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+      
     }
     
     override func viewWillAppear(_ animated: Bool) {
