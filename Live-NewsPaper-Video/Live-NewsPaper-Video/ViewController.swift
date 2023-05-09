107ac3cf6b44e18e1a7c98f24477e7fb9961dff8
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-05-09 21:10:15 +0300

    Add files via upload

--- Live-NewsPaper-Video/Live-NewsPaper-Video/ViewController.swift
+++ Live-NewsPaper-Video/Live-NewsPaper-Video/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             configuration.maximumNumberOfTrackedImages = 1
             
-//            print("images founded")
+            print("images founded")
             
         }
         
@@ -59,9 +59,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let node = SCNNode()
         
         if let imageAnchor = anchor as? ARImageAnchor {
+            
+            let videoNode = SKVideoNode(fileNamed: "harrypotter.mp4")
+            
+            videoNode.play()
+            
+            let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+            
+            videoNode.position = CGPoint(x: videoScene.size.width/2,
+                                         y: videoScene.size.height/2)
+            
+            videoNode.yScale = -1.0
+            
+            videoScene.addChild(videoNode)
+
             let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
             
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            plane.firstMaterial?.diffuse.contents = videoScene
             
             let planeNode = SCNNode(geometry: plane)
             
